8a545ea0c32c76f243967fd610b2b052473db4de
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-09-23 23:38:26 +0800

    Update 3.1
    Fixed the header margin bug!

--- script.js
+++ script.js
@@ -9,32 +9,43 @@ const { createApp, ref } = Vue
     }
   }).mount('#app')
 
-window.addEventListener('resize', function() {
+  window.onload = function() {
 
-  let nav = document.querySelector('nav');
-  let header = document.querySelector('header');
-    
-  let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
-  let navNewHeightVh = navHeightVh + 2;
-  header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
+    let nav = document.querySelector('nav');
+    let header = document.querySelector('header');
+      
+    let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
+    let navNewHeightVh = navHeightVh + 2;
+    header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
 
-
-  let school1 = document.querySelector('#school1');
-  let school2 = document.querySelector('#school2');
-  let school3 = document.querySelector('#school3');
-
-  school1.style.height = 'auto';
-  school2.style.height = 'auto';
-  school3.style.height = 'auto';
-
-  let school1Height = school1.offsetHeight;
-  let school2Height = school2.offsetHeight;
-  let school3Height = school3.offsetHeight;
-
-  let maxHeight = Math.max(school1Height, school2Height, school3Height);
-    
-  school1.style.height = `${maxHeight}px`;
-  school2.style.height = `${maxHeight}px`;
-  school3.style.height = `${maxHeight}px`;
+  };
   
+
+  window.addEventListener('resize', function() {
+
+    let nav = document.querySelector('nav');
+    let header = document.querySelector('header');
+    
+    let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
+    let navNewHeightVh = navHeightVh + 2;
+    header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
+
+    let school1 = document.querySelector('#school1');
+    let school2 = document.querySelector('#school2');
+    let school3 = document.querySelector('#school3');
+
+    school1.style.height = 'auto';
+    school2.style.height = 'auto';
+    school3.style.height = 'auto';
+
+    let school1Height = school1.offsetHeight;
+    let school2Height = school2.offsetHeight;
+    let school3Height = school3.offsetHeight;
+
+    let maxHeight = Math.max(school1Height, school2Height, school3Height);
+    
+    school1.style.height = `${maxHeight}px`;
+    school2.style.height = `${maxHeight}px`;
+    school3.style.height = `${maxHeight}px`;
+
 });
