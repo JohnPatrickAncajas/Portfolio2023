2a56bde8f5457ccdc73af02ecaf831ccad74131a
JohnPatrickAncajas <ancajasjohnpatrickdelapena@gmail.com>
2023-09-26 04:21:06 +0800

    Update 3.2
    Added collapsible part of of project 1!
    
    Merged the JS script into the Vue app!

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-const { createApp, ref } = Vue
+const { createApp, onMounted } = Vue;
 
-  createApp({
-    setup() {
-      const message = ref('Hello vue!')
-      return {
-        message
-      }
-    }
-  }).mount('#app')
+createApp({
+  setup() {
+    const updateHeaderMargin = () => {
+      let nav = document.querySelector('nav');
+      let header = document.querySelector('header');
 
-  window.onload = function() {
-
-    let nav = document.querySelector('nav');
-    let header = document.querySelector('header');
-      
-    let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
-    let navNewHeightVh = navHeightVh + 2;
-    header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
-
-  };
-  
-
-  window.addEventListener('resize', function() {
-
-    let nav = document.querySelector('nav');
-    let header = document.querySelector('header');
+      setTimeout(() => {
+        let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
+        let navNewHeightVh = navHeightVh + 2;
+        header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
+      }, 100);
+    };
     
-    let navHeightVh = (nav.offsetHeight / window.innerHeight) * 100;
-    let navNewHeightVh = navHeightVh + 2;
-    header.style.marginTop = `${(navNewHeightVh * window.innerHeight) / 100}px`;
+    const updateSchoolHeights = () => {
+      let school1 = document.querySelector('#school1');
+      let school2 = document.querySelector('#school2');
+      let school3 = document.querySelector('#school3');
 
-    let school1 = document.querySelector('#school1');
-    let school2 = document.querySelector('#school2');
-    let school3 = document.querySelector('#school3');
+      school1.style.height = 'auto';
+      school2.style.height = 'auto';
+      school3.style.height = 'auto';
 
-    school1.style.height = 'auto';
-    school2.style.height = 'auto';
-    school3.style.height = 'auto';
+      setTimeout(() => {
+        let school1Height = school1.offsetHeight;
+        let school2Height = school2.offsetHeight;
+        let school3Height = school3.offsetHeight;
 
-    let school1Height = school1.offsetHeight;
-    let school2Height = school2.offsetHeight;
-    let school3Height = school3.offsetHeight;
+        let maxHeight = Math.max(school1Height, school2Height, school3Height);
 
-    let maxHeight = Math.max(school1Height, school2Height, school3Height);
-    
-    school1.style.height = `${maxHeight}px`;
-    school2.style.height = `${maxHeight}px`;
-    school3.style.height = `${maxHeight}px`;
+        school1.style.height = `${maxHeight}px`;
+        school2.style.height = `${maxHeight}px`;
+        school3.style.height = `${maxHeight}px`;
+      }, 100);
+    };
 
-});
+    onMounted(() => {
+      updateHeaderMargin();
+      updateSchoolHeights();
+    });
+
+    window.addEventListener('resize', () => {
+      updateHeaderMargin();
+      updateSchoolHeights();
+    });
+  },
+}).mount('#app');
